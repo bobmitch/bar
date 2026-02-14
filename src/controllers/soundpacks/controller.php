0af1a66f01c653b6cc00b3c6543cbad9e43cf5b2
bobmitch <bobmitch@gmail.com>
2026-02-14 17:02:04 +0000

    init: audio controllers

---
+++ src/controllers/soundpacks/controller.php
@@ -0,0 +1,15 @@
+<?php
+use HoltBosse\Alba\Core\{CMS, Content, Controller};
+use HoltBosse\DB\DB;
+
+CMS::Instance()->page->view_configuration_object = json_decode(CMS::Instance()->page->view_configuration);
+
+$segments = CMS::Instance()->uri_segments;
+
+$view = Content::get_view_location(CMS::Instance()->page->view);
+$user = CMS::Instance()->user;
+
+echo "<h1>Sound Packs Controller</h1>";
+
+// $controller = new Controller(realpath(dirname(__FILE__)),$view);
+// $controller->load_view($view); 
