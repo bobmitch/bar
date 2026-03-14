98458db12695c9c4f0f5f1604fd231874fafaccc
bobmitch <bobmitch@gmail.com>
2026-03-14 00:46:30 +0000

    feat: remove redundant stat ticks

--- src/Widgets/BarTracker/eventHandler.js
+++ src/Widgets/BarTracker/eventHandler.js
@@ -281,11 +281,6 @@ class EventHandler {
             data.attackerTeam
         );
 
-        // Update widgets immediately to reflect destruction
-        if (typeof statWidgets !== 'undefined') {
-            statWidgets.tick();
-        }
-
         console.log('💀 Unit destroyed:', {
             victim: data.unitName,
             attacker: data.attackerName,
@@ -334,11 +329,6 @@ class EventHandler {
                 uiManager.updateTeamStatsPanel(myTeam, data);
                 uiManager.updateGameStatus(data.gameTime, gameState.gameState);
             }
-        }
-
-        // Keep streaming widgets in sync with resource/damage stats
-        if (typeof statWidgets !== 'undefined') {
-            statWidgets.tick();
         }
     }
 
